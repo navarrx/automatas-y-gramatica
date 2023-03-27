a2d8936fde3fa077872c9dbf5f230ddf7f2bd8aa
unknown <maximosat02@gmail.com>
2023-03-27 10:06:18 -0300

    agregado

---
+++ Ejercicio_2.py
@@ -0,0 +1,22 @@
+def solve(string):
+    numeros = string.split()
+    resultado_parcial = 0
+    operacion = None
+    total = 0
+    for i in range(0, len(numeros)):
+        if i % 2 == 0:
+            numero = int(numeros[i])
+            if operacion == '*':
+                resultado_parcial *= numero
+            else:
+                resultado_parcial = numero
+        else:
+            operacion = numeros[i]
+            if operacion == '+':
+                total += resultado_parcial
+    total += resultado_parcial
+    return total
+
+operacion = "2 * 2 * 2 + 32 * 2"
+resultado = solve(operacion)
+print(resultado)
