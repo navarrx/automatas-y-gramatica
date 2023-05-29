103a2a8f8405b330051b2bbcae7f83ca5e2b031c
navarrx <sa.navarro@alumno.um.edu.ar>
2023-05-29 19:02:27 -0300

    Corregimos ejercicio URL que presentaba error

---
+++ url_regex_corrected.py
@@ -0,0 +1,13 @@
+import re
+
+url_regex = re.compile(r'^(?:http[s]?://)?(?:www\.)?[\w.-]+\.[a-zA-Z]{2,}(?:/[\w.-]*)*(?:\?[\w=&]*)?$')
+
+def url_evaluator():
+    url = str(input("\nInsert your URL here: "))
+    if re.fullmatch(url_regex, url):
+        print(f'The url "{url}" is approved!')
+    else:
+        print(f'The url "{url}" is denied. Please try again')
+
+
+url_evaluator()
